19b1d058cbd9ba807df0808cae7e6356790a6a1c
Dave Rolsky <autarch@urth.org>
2017-04-30 20:00:20 -0500

    Add a basic DESCRIPTION to the main module

--- lib/App/CISetup.pm
+++ lib/App/CISetup.pm
@@ -10,3 +10,13 @@ our $VERSION = '0.01';
 # ABSTRACT: Command line tools to generate and update Travis and AppVeyor configs for Perl libraries
 
 __END__
+
+=pod
+
+=head1 DESCRIPTION
+
+This distro includes two command-line tools, L<update-travis-yml.pl> and
+L<update-appveyor-yml.pl>. They update Travis and AppVeyor YAML config files
+with some opinionated defaults. See the docs for the respective scripts for
+more details.
+
