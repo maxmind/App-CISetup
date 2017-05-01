d76b746f2c0dc2067e62af600e1158c4d1bef4c7
Dave Rolsky <autarch@urth.org>
2017-04-30 20:00:29 -0500

    Add docs to update-travis-yml.pl

--- bin/update-travis-yml.pl
+++ bin/update-travis-yml.pl
@@ -11,7 +11,7 @@ __END__
 
 =pod
 
-=head1 NAME - App::CISetup::Travis::ConfigUpdater
+=head1 NAME - update-travis-yml.pl
 
 =head1 DESCRIPTION
 
@@ -19,21 +19,17 @@ Update existing .travis.yml files with Slack and notification settings
 
 =head1 GETTING STARTED
 
-You'll need to have the Travis CLI installed.  On a linux box this would be
-something like
+You'll need a non-empty F<.travis.yml> file in your repo to get started. This
+is enough to get the tool to generate a usable config.
 
-    sudo apt-get install ruby1.9.1-dev
-    sudo gem install travis -v 1.8.2 --no-rdoc --no-ri
+  ---
+  sudo: false
+  before_install:
+    - travis-perl
 
-You'll need a non-empty .travis.yml file in your repo to get started.  Something
-like this is enough:
-
-   ---
-   sudo: false
-
-Though if you are building a Perl repository you'll need to add a before
-install step so that the config updater will recognize this is a Perl
-distro and expand to the full config
+Though if you are building a Perl repository you'll need to add a
+C<before_install> step so that the config updater will recognize this is a
+Perl distro and expand to the full config
 
     ---
     sudo: false
@@ -44,10 +40,160 @@ Now you just need to invoke this script with a --dir for your repository:
 
     update-travis-yml.pl --dir ~/checkouts/MyRepoName
 
-If you want email or slack notification you'll need to pass a few more param:
+If you want email or slack notification you'll need to pass a few more
+arguments:
 
     update-travis-yml.pl \
        --github-user example \
        --slack-key o8PZMLqZK6uWVxyyTzZf4qdY \
        --email-address example@example.org
 
+=head1 THE CONFIG
+
+The updated config is based in part of the config that's already there, so the
+tools starts by reading in the existing config. It will use this as the guide
+for some decisions about what to update, as detailed below.
+
+This config is all based on using the travis-perl helper scripts. See
+L<https://github.com/travis-perl/helpers> for details.
+
+Here's a step-by-step guide to the generated Travis config and what it does:
+
+=head2 C<sudo> and C<addons>
+
+By default, C<sudo> will be disabled for the Travis run. This makes Travis
+faster. However, if an existing C<before_install> or C<install> block invokes
+C<sudo>`, then sudo will be enabled.
+
+When C<sudo> is disabled, the C<addons> block will be updated to include
+C<aspell> and C<aspell-en> for the benefit of L<Test::Spelling>.
+
+If C<sudo> is enabled, then you'll need to make sure your config installs any
+Debian packages which are needed.
+
+=head2 C<before_install>, C<install>, and C<script>
+
+If this exists and does not mention either C<travis-perl> (the new name) or
+C<perl-travis-helper> (the old name), then these blocks will be left as-is.
+
+If the travis-perl helpers I<are> referenced, the following updates are done:
+
+If the C<script> block is more than 3 lines long I<and> either the C<install>
+block is longer than 2 line I<or> the C<install> block does not contain a call
+to the travis-perl C<cpan-install>, then the C<before_install> block is
+updated to include these lines:
+
+    - git clone git://github.com/travis-perl/helpers ~/travis-perl-helpers
+    - source ~/travis-perl-helpers/init
+
+If there are existing travis-perl C<clone> and C<source> lines, these will be
+replaced with the two lines above. Otherwise these two lines will be inserted
+at the beginning of the block.
+
+This is how you would start using the travis-perl helpers in the non-auto
+(long) config.
+
+If the C<script> and C<install> blocks don't match the aforementioned
+conditions, then the C<instal> and C<script> blocks are deleted entirely and
+the C<before_install> block is updated to contain this line:
+
+    - eval $(curl https://travis-perl.github.io/init) --auto
+
+If there is an existing travis-perl C<eval> line, this will be replaced with
+the line above. Otherwise this line will be inserted at the beginning of the
+block.
+
+=head2 C<perl> and C<matrix>
+
+The C<perl> block will be updated based on the following rules:
+
+If your distro does not have XS and you did not force the use of threaded
+Perls, then you get a block like this:
+
+    perl:
+      - blead
+      - dev
+      - '5.24'
+      - '5.22'
+      - '5.20'
+      - '5.18'
+      - '5.16'
+      - '5.14'
+
+If the distro has XS code or you pass the C<--force-threaded-perls>
+command-line argument, then you will get a block with every Perl from 5.14 to
+the latest stable release, plus dev and blead, in both threaded and unthreaded
+forms. This will look something like this:
+
+    perl:
+      - blead
+      - blead-thr
+      - dev
+      - dev-thr
+      - 5.24.1
+      - 5.24.1-thr
+      - 5.22.3
+      - 5.22.3-thr
+      - 5.20.3
+      - 5.20.3-thr
+      - 5.18.3
+      - 5.18.3-thr
+      - 5.16.3
+      - 5.16.3-thr
+      - 5.14.4
+      - 5.14.4-thr
+
+In either case, you will also get a C<matrix> block that is configured to
+allow failures for all blead runs. It will also include a run with
+C<COVERAGE=1> in the environment against the latest stable Perl version, so
+something like this:
+
+    matrix:
+      allow_failures:
+        - perl: blead
+      include:
+        - env: COVERAGE=1
+          perl: '5.24'
+
+=head2 C<notifications>
+
+If you pass an C<--email-address> or C<--slack-key> command-line argument,
+then this block will be updated. For email, notifications will be sent on all
+failures and on changes.
+
+If you pass a slack key the C<travis> command line tool will be executed to
+encrypt the key and it will be added to the config. If you have an existing
+secure key it will not be updated, because the C<travis> tool generates a new
+encrypted key every time it's invoked, leading to annoying churn.
+
+=head1 ARGUMENTS
+
+This script accepts the following command-line arguments:
+
+=head2 --force-threaded-perls
+
+Force the inclusion of both threaded and unthreaded Perls in the generated
+config, regardless of whether the distro has XS or not.
+
+=head2 --slack-key
+
+A Slack key to use for Slack notifications. If you pass this you must also
+pass C<--github-user>.
+
+=head2 --github-user
+
+Your github user name. This is required if you pass C<--slack-key>.
+
+=head2 --email-address
+
+The email address to which notifications should be sent. This is optional, and
+if you don't provide it, then no notification emails will be configured (but
+the default Travis notifications will still be in place).
+
+You'll need to have the Travis CLI installed. On a linux box this would be
+something like
+
+    sudo apt-get install ruby1.9.1-dev
+    sudo gem install travis -v 1.8.2 --no-rdoc --no-ri
+
+=cut
