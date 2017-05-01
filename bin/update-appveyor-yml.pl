becf8251a65ea11bacd508d5e1ea3b56c58a246f
Dave Rolsky <autarch@urth.org>
2017-04-30 20:24:23 -0500

    Add minimal docs for AppVeyor script
    The AppVeyor stuff needs a bit more work before it's really useful.

--- bin/update-appveyor-yml.pl
+++ bin/update-appveyor-yml.pl
@@ -6,3 +6,20 @@ use warnings;
 use App::CISetup::AppVeyor::ConfigUpdater;
 
 exit App::CISetup::AppVeyor::ConfigUpdater->new_with_options->run;
+
+__END__
+
+=pod
+
+=head1 NAME - update-appveyor-yml.pl
+
+=head1 DESCRIPTION
+
+This script updates existing appveyor.yml files with various settings from the
+command line. Currently all this does is update the notifications block for
+Slack and email notifications.  It also reorders the top-level keys in the
+YAML file and does some other minor cleanups.
+
+B<This script needs more work before it's really useful. Patches are welcome.>
+
+=cut
