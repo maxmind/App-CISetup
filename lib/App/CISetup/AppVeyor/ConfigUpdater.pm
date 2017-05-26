347d3c26c6ddd3b47bbb25e026649fc4cf060960
Dave Rolsky <autarch@urth.org>
2017-05-26 11:47:47 -0500

    Bump version after release

--- lib/App/CISetup/AppVeyor/ConfigUpdater.pm
+++ lib/App/CISetup/AppVeyor/ConfigUpdater.pm
@@ -5,7 +5,7 @@ use warnings;
 use namespace::autoclean;
 use autodie qw( :all );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use App::CISetup::AppVeyor::ConfigFile;
 use App::CISetup::Types qw( Bool Str );
