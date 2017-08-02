432bf491976d081138462918579249b65cd2fca0
Dave Rolsky <autarch@urth.org>
2017-08-02 14:21:41 -0400

    Bump version after release

--- bin/setup-travis-yml.pl
+++ bin/setup-travis-yml.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use App::CISetup::Travis::ConfigUpdater;
 
--- lib/App/CISetup.pm
+++ lib/App/CISetup.pm
@@ -7,7 +7,7 @@ use warnings;
 # For use autodie qw( :all ) elsewhere
 use IPC::System::Simple ();
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 1;
 
